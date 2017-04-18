423df985a55fcf99e98191cb4d2505b6830cb446
Mark Saltarelli <Mark@Marks-MacBook-Pro.local>
2017-04-18 15:32:05 -0400

    homework week 11 + 12

--- script.js
+++ script.js
@@ -1,134 +1,132 @@
 // LIST ARRAY IS WHERE OUR DATA FOR THIS APPLICATION LIVES
-var listArray = [
-  { name: "Books to Read",
-    items: [
-      "Hitchhiker's Guide to Galaxy",
-      "Walden",
-      "The Elephant, the Tiger, and the Cell Phone"
-    ]
-  },
-  { name: "Things to Do",
-    items: [
-      "Read",
-      "Cry"
-    ]
-  }
+var listArray = [{
+        name: "Books to Read",
+        items: [
+            "Hitchhiker's Guide to Galaxy",
+            "Walden",
+            "The Elephant, the Tiger, and the Cell Phone"
+        ]
+    },
+    {
+        name: "Things to Do",
+        items: [
+            "Read",
+            "Cry",
+            "Give up"
+        ]
+    }
 ];
-
 var selectedList = 0;
 var listDiv = document.getElementById("lists");
 var itemDiv = document.getElementById("list-items");
 var addListButton = document.getElementById("add-list-button");
 var addItemButton = document.getElementById("add-item-button");
 
+//select a list
+
+listDiv.addEventListener("click", function(e){
+    selectedList = e.target.dataset.index;
+
+    var listHTMLElements = listDiv.querySelectorAll("a");
+    listHTMLElements.forEach(function(list, i) {
+        list.classList.remove("active");
+        if (selectedList == i) {
+            list.classList.add("active");
+        }
+    });
+    updateItemsForSelectedList();
+});
+
 // FUNCTIONS TO UPDATE THE HTML PAGE WITH RESPECT TO DATA
-function updateLists(){
-	while (listDiv.hasChildNodes()){ //while loop will keep going as long as they have HTML elements
-		listDiv.removeChild(listDiv.lastChild);
-	}
-	function addHTMLForList(list,index){ //function constantly called with forEach loop
-	var aElement = document.createElement("a");
-	aElement.classList.add("list-group-item");
-	aElement.classList.add("list-group");
-	aElement.classList.add("list-group-item-action");
-	
-	aElement.setAttribute("data-index", index); //index is tracking the number of the element
-	aElement.setAttribute("data-list-name", list.name);
-	
-	var textNode = document.createTextNode(list.name); //giving user ability to name list. This is how data is structured above
-	aElement.appendChild(textNode);
-	
-	listDiv.appendChild(aElement); //append child adds additional HTML element to another parent HTML element
-	}
-	listArray.forEach(addHTMLForList);
+function updateLists() {
+    while (listDiv.hasChildNodes()) {
+        listDiv.removeChild(listDiv.lastChild);
+    }
+
+    listArray.forEach(function(list, i) {
+        var aElement = document.createElement("a");
+        aElement.classList.add("list-group-item");
+        aElement.classList.add("list-group-item-action");
+        aElement.classList.add("list");
+		if (i == selectedList) {
+			aElement.classList.add("active");
+		}
+
+        aElement.setAttribute("data-index", i);
+
+        var textNode = document.createTextNode(list.name);
+        aElement.appendChild(textNode);
+
+        listDiv.appendChild(aElement);
+    });
+}
+
+function updateItemsForSelectedList() {
+    while (itemDiv.hasChildNodes()) {
+        itemDiv.removeChild(itemDiv.lastChild);
+    }
+
+    var listItemArray = listArray[selectedList].items;
+    listItemArray.forEach(function(item, i) {
+		var aElement = document.createElement("a");
+        aElement.classList.add("list-group-item");
+        aElement.classList.add("list-group-item-action");
+        aElement.classList.add("list");
+		aElement.setAttribute("data-index", i);
+		var textNode = document.createTextNode(item);
+		aElement.appendChild(textNode);
+		itemDiv.appendChild(aElement);
+    });
 }
 
 updateLists();
-
-function updateItems(){
-	while (itemDiv.hasChildNodes()){ //while loop will keep going as long as they have HTML elements
-		itemDiv.removeChild(itemDiv.lastChild);
-	}
-	function addHTMLForList(list,index){ //function constantly called with forEach loop
-	var aElement = document.createElement("a");
-	aElement.classList.add("list-group-item");
-	aElement.classList.add("list-group");
-	aElement.classList.add("list-group-item-action");
-	
-	aElement.setAttribute("data-index", index); //index is tracking the number of the element
-	aElement.setAttribute("data-list-name", list.items);
-	
-	var textNode = document.createTextNode(list.items); //giving user ability to name list. This is how data is structured above
-	aElement.appendChild(textNode);
-	
-	itemDiv.appendChild(aElement); //append child adds additional HTML element to another parent HTML element
-	}
-	listArray.forEach(addHTMLForList);
-}
-
-updateItems();
+updateItemsForSelectedList();
 
 // ADDING TO LIST
-addListButton.addEventListener("click", function (e){ //button is a submit button, if form action is not defined, it will refresh page
-	e.preventDefault(); //prevents default behavior described above
-	 
-	var input = document["add-list-form"]["list-name-input"]; //allows us to access value without calling element by ID
-//	console.log(input.value);
-	var newListName = input.value;
-	if (newListName.length > 2){
-		var newList = { name: newListName, //var newList is the list element, we want to add this to the array
-					   items: []};
-		listArray.push(newList); //adding list element to the array
-		updateLists(); //call the same function again to populate the list array
-		closePopups();
-		
-	}else {
-		alert("List Name Not Valid!");
-	}
+addListButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    var listName = document["add-list-form"]["list-name-input"].value;
+    if (listName.length >= 3) {
+        var newList = {
+            name: listName,
+            items: []
+        };
+        listArray.push(newList);
+        updateLists();
+    } else {
+        alert("Please enter a valid list name: Atleast 3 characters");
+    }
 });
-
-addItemButton.addEventListener("click", function (e){ //button is a submit button, if form action is not defined, it will refresh page
-	e.preventDefault(); //prevents default behavior described above
-	 
-	var input = document["add-item-form"]["list-item-input"]; //allows us to access value without calling element by ID
-//	console.log(input.value);
-	var newItemName = input.value;
-	if (newItemName.length > 2){
-		var newListItem = { items: [newItemName]}; //var newList is the list element, we want to add this to the array
-		listArray.push(newListItem); //adding list element to the array
-		updateItems(); //call the same function again to populate the list array
-		closePopups();
-		
-	} else {
-		alert("List Name Not Valid!");
-	}
-});
-
 // ADDING TO LIST ITEMS
-
-
+addItemButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    var currentList = listArray[selectedList];
+    var itemArray = currentList.items;
+	var inputValue = document["add-item-form"]["item-name-input"].value;
+	itemArray.push(inputValue);
+	updateItemsForSelectedList();
+});
 
 // POP-UP HANDLING CODE
 var buttonsArray = document.querySelectorAll(".popup-button");
-// querySelectorAll returns a DOMTokenList and not an Array (which includes methods like forEach)
-buttonsArray = Array.from(buttonsArray); // Conevrting DOMTokenList to an Array
+buttonsArray = Array.from(buttonsArray);
 
 buttonsArray.forEach(function(button) {
-  button.addEventListener("click", function() {
-    var popup = document.getElementById(this.dataset.popupid); 
-    // The data attributes can be accessed by .dataset variable which is part of the DOMElement (check HTML for buttonsArray)
-    popup.style.display = "flex";
-  });
+    button.addEventListener("click", function() {
+        var popup = document.getElementById(this.dataset.popupid);
+        popup.style.display = "flex";
+    });
 });
 
 var closeButton = document.querySelectorAll(".close");
 closeButton.forEach(function(button, i) {
-  button.addEventListener("click", closePopups);
+    button.addEventListener("click", closePopups);
 });
 
 function closePopups() {
-  var popupsArray = Array.from(document.querySelectorAll(".popup"));
-  popupsArray.forEach(function(popup) {
-    popup.style.display = "none";
-  });
+    var popupsArray = Array.from(document.querySelectorAll(".popup"));
+    popupsArray.forEach(function(popup) {
+        popup.style.display = "none";
+    });
 }
+
